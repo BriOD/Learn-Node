45458e763e68a680da55dc1ed06a9d8c1aa48a50
BriOD <briodonoghue@gmail.com>
2018-06-04 22:21:31 -0400

    built put User

--- tb/models/User.js
+++ tb/models/User.js
@@ -7,7 +7,22 @@ const mongodbErrorHandler = require('mongoose-mongodb-errors');
 const passportLocalMongose = require('passport-local-mongoose');
 
 const userSchema = new Schema({
-
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        validate: [validator.isEmail, 'Invalid Email Address.'],
+        required: 'Please supply an email address.'
+    },
+    name: {
+        type: String,
+        required: 'Please supply a name.',
+        trim: true
+    }
 });
+
+userSchema.plugin(passportLocalMongose, { usernameField: 'email' });
+userSchema.plugin(mongodbErrorHandler);
 
 module.exports = mongoose.model('User', userSchema);
